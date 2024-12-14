a995ec5ffab3f626c1027500f9b0d7dffc21903c
Mengsroin Heng <hengmengsroin@gmail.com>
2024-12-14 22:33:11 +0700

    feat: Add web support for Flutter Thermal Printer plugin and update dependencies

---
+++ lib/flutter_thermal_printer_web.dart
@@ -0,0 +1,10 @@
+import 'dart:html' as html;
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+class FlutterThermalPrinterPluginWeb {
+  static void registerWith(Registrar registrar) {
+    // Register the plugin
+  }
+
+  // Add your web-specific implementation here
+}
