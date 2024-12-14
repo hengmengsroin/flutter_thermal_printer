7ef5f552ca2b55543bc4147a884642a9f92faf2a
Mengsroin Heng <hengmengsroin@gmail.com>
2024-12-14 22:52:46 +0700

    feat: Refactor Flutter Thermal Printer plugin for web support and enhance method implementations

--- lib/flutter_thermal_printer_web.dart
+++ lib/flutter_thermal_printer_web.dart
@@ -1,10 +1,85 @@
-import 'dart:html' as html;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_thermal_printer/utils/printer.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'flutter_thermal_printer_platform_interface.dart';
 
-class FlutterThermalPrinterPluginWeb {
+class FlutterThermalPrinterPlugin extends FlutterThermalPrinterPlatform {
+  static const String platformChannel = "flutter_thermal_printer";
+  static late MethodChannel channel;
+  bool supported = false;
   static void registerWith(Registrar registrar) {
     // Register the plugin
+    channel =
+        MethodChannel(platformChannel, const StandardMethodCodec(), registrar);
+    final instance = FlutterThermalPrinterPlugin();
+    channel.setMethodCallHandler(instance.handleMethodCall);
   }
 
-  // Add your web-specific implementation here
+  Future<dynamic> handleMethodCall(MethodCall call) async {
+    if (!supported) return;
+    switch (call.method) {
+      default:
+        throw PlatformException(
+            code: 'Unimplemented',
+            details: "The flutter_tts plugin for web doesn't implement "
+                "the method '${call.method}'");
+    }
+  }
+
+  @visibleForTesting
+  final methodChannel = const MethodChannel('flutter_thermal_printer');
+
+  @override
+  Future<String?> getPlatformVersion() async {
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
+    return version;
+  }
+
+  @override
+  Future<dynamic> startUsbScan() async {
+    return await methodChannel.invokeMethod('getUsbDevicesList');
+  }
+
+  @override
+  Future<bool> connect(Printer device) async {
+    return await methodChannel.invokeMethod('connect', {
+      "vendorId": device.vendorId.toString(),
+      "productId": device.productId.toString(),
+    });
+  }
+
+  @override
+  Future<bool> printText(Printer device, Uint8List data, {String? path}) async {
+    return await methodChannel.invokeMethod('printText', {
+      "vendorId": device.vendorId.toString(),
+      "productId": device.productId.toString(),
+      "data": List<int>.from(data),
+      "path": path ?? "",
+    });
+  }
+
+  @override
+  Future<bool> isConnected(Printer device) async {
+    return await methodChannel.invokeMethod('isConnected', {
+      "vendorId": device.vendorId.toString(),
+      "productId": device.productId.toString(),
+    });
+  }
+
+  @override
+  Future<dynamic> convertImageToGrayscale(Uint8List? value) async {
+    return await methodChannel.invokeMethod('convertimage', {
+      "path": List<int>.from(value!),
+    });
+  }
+
+  @override
+  Future<bool> disconnect(Printer device) async {
+    return await methodChannel.invokeMethod('disconnect', {
+      "vendorId": device.vendorId.toString(),
+      "productId": device.productId.toString(),
+    });
+  }
 }
